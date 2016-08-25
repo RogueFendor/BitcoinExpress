bb9ef1a564acac4e21ae455244846a2a2e3f712a
CaptainFlint <captainflint@CaptainFlint.LibStaff.ul.campus>
2016-08-25 13:58:32 +0100

    rough Wiki edit

--- src/js/app.js
+++ src/js/app.js
@@ -10,19 +10,48 @@ var app ={};
 // A $( document ).ready() block.
 $( document ).ready(function() {
     console.log( "ready!" );
-   
+  
    // $("#wiki-sidenav").toggle("slide");
   // -------> Event handlers 
   
      // # Mobile view
   $(".button-collapse").sideNav();
   $(".brand-logo").sideNav();
-     // # slide index in/out
-  //$("#wiki-index").click(function(){
-  	//console.log( "clicked!" );
-  	//$("#wiki-sidenav").toggle("slide");
-      
-  //});
-
-
+     
+     // #----> scroll wiki
+  $("#w1").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 2000);
+  });
+  $("#w2").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#setup").offset().top
+    }, 2000);
+  });
+  $("#w3").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#android").offset().top
+    }, 2000);
+  });
+  $("#w4").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#dashboard").offset().top
+    }, 2000);
+  });
+  $("#w5").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#publishing").offset().top
+    }, 2000);
+  });
+  $("#w6").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#subscribing").offset().top
+    }, 2000);
+  });
+  $("#w7").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#IM").offset().top
+    }, 2000);
+  });
 });
